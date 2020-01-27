e31db425d09bc85599493756d3593ce70bf2a05d
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-01-27 15:19:15 +1100

    Resize correct partition

--- sd-image-rockchip.nix
+++ sd-image-rockchip.nix
@@ -120,7 +120,7 @@ in
         sfdisk -d $bootDevice | ${pkgs.gnugrep}/bin/grep -v '^last-lba:' | sfdisk --no-reread $bootDevice
 
         # Resize the root partition and the filesystem to fit the disk
-        echo ",+," | sfdisk -N2 --no-reread $bootDevice
+        echo ",+," | sfdisk -N1 --no-reread $bootDevice
         ${pkgs.parted}/bin/partprobe
         ${pkgs.e2fsprogs}/bin/resize2fs $rootPart
 
